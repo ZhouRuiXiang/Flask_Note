24877b048b12f0a175fbcf372a8e4e2faed3c9c4
Linux学习笔记 <1215686977@qq.com>
2020-01-16 21:40:01 +0800

    add third-party

--- UEditor_demo/config.py
+++ UEditor_demo/config.py
@@ -3,8 +3,9 @@ import os
 
 UEDITOR_UPLOAD_PATH = os.path.join(os.path.dirname(__file__), 'images')
 # print(UEDITOR_UPLOAD_PATH)
-UEDITOR_UPLOAD_TO_QINIU = True
+UEDITOR_UPLOAD_TO_QINIU = False
 UEDITOR_QINIU_ACCESS_KEY = "_MZMe1XwdcsqFnhpoqd2OtUC_VVaUr4ku6W1TIur"
 UEDITOR_QINIU_SECRET_KEY = "QWfV-5wpbO6Of-f7JQ0R2K0phljk79Qd7KumSviJ"
 UEDITOR_QINIU_BUCKET_NAME = "starspace"
 UEDITOR_QINIU_DOMAIN = "http://q3w8oarc6.bkt.clouddn.com/"
+
